075742e1a7311bc61b26b65ac78f06b61cc67fb3
Domenico Manna <mannadomenico2849@gmail.com>
2019-04-06 20:06:54 -0400

    Fix hamburger menu bug

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
-let hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const mainNavigationMenu = document.querySelector('.main-navigation');
 
-hamburgerMenu.addEventListener('click', () => {
-    let mainNavigationMenu = document.querySelector('.main-navigation');
+loadEventListeners();
 
-    if (mainNavigationMenu.classList.contains('enable')){
+function loadEventListeners(){
+    hamburgerMenu.addEventListener('click', showOrHideHamburgerMenu);
+    window.addEventListener("resize", cleanUpMainNavigationClassList);
+}
+
+function showOrHideHamburgerMenu() {
+
+    if (mainNavigationMenu.classList.contains('enable')) {
         mainNavigationMenu.classList.remove('enable');
         mainNavigationMenu.classList.add('disable');
     }
-    else{
+    else {
         mainNavigationMenu.classList.remove('disable');
         mainNavigationMenu.classList.add('enable');
     }
-    
-})
+
+}
+
+function cleanUpMainNavigationClassList() {
+    var width = document.documentElement.clientWidth;
+    if (width > 768) {
+        mainNavigationMenu.classList.remove('enable');
+        mainNavigationMenu.classList.remove('disable');
+    }
+}
