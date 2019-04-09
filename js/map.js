b51c6c9b646b988377f114cd13c67d0ddbe34a3d
Domenico Manna <mannadomenico2849@gmail.com>
2019-04-09 18:45:25 -0400

    Add location on google map

---
+++ js/map.js
@@ -0,0 +1,15 @@
+function initializeMap() {
+    let mapElement = document.querySelector(".contact__map");
+    let greenich = {lat: 41.027054, lng: -73.620071};
+
+    mapElement = new google.maps.Map(mapElement, {
+    center: greenich,
+    zoom: 8
+  });
+
+  var marker = new google.maps.Marker({
+    position: greenich,
+    map: mapElement,
+    title: 'Hello World!'
+  });
+}
